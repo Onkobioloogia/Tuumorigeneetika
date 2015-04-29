47e8059c3f380b16aa308572d8b9b0fc7202ae36
Taavi Päll <tapa741@gmail.com>
2015-04-29 16:34:58 +0300

    publishing deck

--- src/munge.R
+++ src/munge.R
@@ -2,12 +2,11 @@
 # dim(europe)
 library(magrittr)
 library(plyr);library(dplyr)
-# europe <- read.csv("data/concord-2/europe.csv",header = FALSE)
 
 munge <- function(x) {
   sites <- c("Stomach","Colon","Rectum","Liver","Lung",
-             "Breast","Cervix","Ovary","Prostate",
-             "Leukaemia.adult","ALL.children")
+             "Breast","Cervix","Ovary","Prostate","Leukaemia","ALL")
+  x %<>% "["(apply(.,2, function(y) !all(is.na(y))))
   colnames(x) <- c("Period",sites)
   newx <- x[!x$Period%in%"",]
   newx$Period %>% 
@@ -16,7 +15,7 @@ munge <- function(x) {
     rep(.,each = 4) %>%
     data.frame(Country=.,newx) %>% 
     .[!apply(., 1, function(x) x[1]==x[2]),] %>% {
-      values <- "["(.,,3:11) %>% 
+      values <- "["(.,,3:ncol(.)) %>% 
         apply(.,2, .%>%as.character%>%as.numeric)
       "["(.,,1:2) %>% cbind(values)}
 }
@@ -29,12 +28,23 @@ concord <- list.files(path = "data/concord-2/", pattern = ".csv", full.names = T
          Period=sub("([0-9-]+) ","\\1",Period)) %>%
   droplevels
 
-concord %>% summary
-
 library(reshape2)
 library(ggplot2)
+# library(ggthemes)
+
 concord %>% 
   melt %>% 
-  ggplot(aes(x=factor(Period),y=value)) +
-  geom_boxplot() +
-  facet_grid(variable~Region,scales = "free_x")
+  ggplot(aes(x=Period,y=value)) +
+  geom_line(aes(group=Country)) +
+  geom_point(aes(group=Country)) +
+  facet_grid(variable~Region,scales = "free_x") +
+  scale_x_discrete(breaks=c("1995-99", "2000-04", "2005-09"),
+                     labels=c("'95-'99", "'00-'04", "'05-'09")) +
+  ylab(bquote(5-year~age-standardised~net~survival)) +
+  xlab(bquote(Period~group("(",years,")"))) +
+  ggtitle(bquote(atop(Global~surveillance~of~cancer~survival~1995-2009,
+                      list(paste(25.7,M)~patients,67~countries~group("(", CONCORD-2,")"))))) +
+  theme(axis.text.x = element_text(angle=45,hjust=1))
+
+# ggsave("concord-2.pdf")
+# ggsave("concord-2.png")
